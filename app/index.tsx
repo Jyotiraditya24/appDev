33fa173a50b5bba0e88bf3f60bdef7fe52b35e88
jyotiraditya <jyotiraditya24@gmail.com>
2024-10-05 19:14:55 +0530

    andriod dev commit day 1

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+const Index = () => {
+  return (
+    <View style={{ backgroundColor: "blue", flex: 1 ,justifyContent:"center", alignItems:"center"}}>
+      <Text style={{ backgroundColor: "red" }}>Hello</Text>
+      <Text style={{ backgroundColor: "red" }}>World</Text>
+    </View>
+  );
+};
+
+export default Index;
